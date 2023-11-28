9e2116e035e99b97a643863a0f949f944f379811
Sarah Martel <57870970+sarahmarie23@users.noreply.github.com>
2023-11-28 13:40:44 -0800

    Delete Constants.java

--- Constants.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-public class Constants {
-    public static final double INITIAL_PHEROMONE_VALUE = 0.01;
-    public static final int NUM_ANTS = 20;
-    public static final int NUM_ITERATIONS = 100;
-    public static final double EVAPORATION_RATE = 0.5;
-    public static final int NUM_VERTICES = 10;
-}
